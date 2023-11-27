6247cca5ffd8cdc9afaf1d0d7accfdc4fc9385de
Firdatul A'yuni <107979861+Firdatulayuni@users.noreply.github.com>
2023-11-27 07:34:18 +0700

    psdproject2.py

--- psdproject2.py
+++ psdproject2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import librosa
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
